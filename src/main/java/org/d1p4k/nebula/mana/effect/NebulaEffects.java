00281780b3a30bab0fe569eb8f23d28048824269
D1p4k <82884007+D1p4k@users.noreply.github.com>
2022-10-17 13:46:41 +0200

    Update NebulaEffects.java

--- src/main/java/org/d1p4k/nebula/mana/effect/NebulaEffects.java
+++ src/main/java/org/d1p4k/nebula/mana/effect/NebulaEffects.java
@@ -3,7 +3,10 @@ package org.d1p4k.nebula.mana.effect;
 import net.minecraft.entity.effect.InstantStatusEffect;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.item.Items;
 import net.minecraft.potion.Potion;
+import net.minecraft.potion.Potions;
+import net.minecraft.recipe.BrewingRecipeRegistry;
 import net.minecraft.util.registry.Registry;
 
 public class NebulaEffects {
@@ -16,17 +19,30 @@ public class NebulaEffects {
 
 
     public static void init() {
+        registerPotionEffects();
+        registerPotions();
+        registerPotionRecipe();
+    }
+
+    private static void registerPotionEffects() {
         register(34, "nebula:instant_mana", MANA_INSTANT_EFFECT);
-
-        MANA_INSTANT = register("instant_mana", new Potion("Instant_Mana", new StatusEffectInstance(MANA_INSTANT_EFFECT, 1, 1)));
         register(35, "nebula:mana_regeneration", MANA_REGENERATION_EFFECT);
+    }
 
-        MANA_REGENERATION = register("mana_regeneration", new Potion("Mana_Regeneration",
-                new StatusEffectInstance(MANA_REGENERATION_EFFECT, 5)));
-        STRONG_MANA_REGENERATION = register("strong_mana_regeneration", new Potion("Strong_Mana_Regeneration",
-                new StatusEffectInstance(MANA_REGENERATION_EFFECT, 5, 1)));
+    private static void registerPotions() {
+        MANA_INSTANT = register("instant_mana", new Potion("Instant_Mana", new StatusEffectInstance(MANA_INSTANT_EFFECT, 1, 1)));
+        MANA_REGENERATION = register("mana_regeneration", new Potion("Mana_Regeneration", new StatusEffectInstance(MANA_REGENERATION_EFFECT, 5)));
+        STRONG_MANA_REGENERATION = register("strong_mana_regeneration", new Potion("Strong_Mana_Regeneration", new StatusEffectInstance(MANA_REGENERATION_EFFECT, 5, 1)));
 
     }
+
+    private static void registerPotionRecipe() {
+        BrewingRecipeRegistry.registerPotionRecipe(Potions.AWKWARD, Items.LAPIS_LAZULI, MANA_REGENERATION);
+        BrewingRecipeRegistry.registerPotionRecipe(MANA_REGENERATION, Items.BEDROCK, MANA_REGENERATION);
+    }
+
+
+
 
     private static Potion register(String name, Potion potion) {
         return Registry.register(Registry.POTION, name, potion);
