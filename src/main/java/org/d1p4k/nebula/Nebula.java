bd82fa1a83695196409c9ab1934f9b0222d3d985
D1p4k <82884007+D1p4k@users.noreply.github.com>
2022-12-21 17:44:50 +0100

    Update to 1.19.3 (#1)
    * Remove Effects and SpellBookItems.
    
    * Update Version to 1.7
    
    * Update Version to 1.19.3 + 1.7.1
    
    * Change dependency to be 1.19.3
    
    * Adapt to new Constructor

--- src/main/java/org/d1p4k/nebula/Nebula.java
+++ src/main/java/org/d1p4k/nebula/Nebula.java
@@ -5,14 +5,12 @@ import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.loader.api.FabricLoader;
 import org.d1p4k.nebula.api.NebulaSpellRegisterEntrypoint;
 import org.d1p4k.nebula.commands.NebluaCommand;
-import org.d1p4k.nebula.mana.effect.NebulaEffects;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Nebula implements ModInitializer {
 
     public static final Logger LOGGER = LoggerFactory.getLogger("Nebula");
-
 
 
     @Override
@@ -23,8 +21,6 @@ public class Nebula implements ModInitializer {
         FabricLoader.getInstance().getEntrypointContainers("nebula", NebulaSpellRegisterEntrypoint.class).forEach(entrypoint -> {
             entrypoint.getEntrypoint().registerSpells();
         });
-        NebulaEffects.init();
-
     }
 
 
