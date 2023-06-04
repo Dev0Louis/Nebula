12f3f96a9ef59d58df694b612ba3eeac93de560a
D1p4k <82884007+D1p4k@users.noreply.github.com>
2023-06-04 16:32:30 +0200

    Remove old Config that isn´t used anymore.

--- src/main/java/dev/louis/nebula/NebulaClient.java
+++ src/main/java/dev/louis/nebula/NebulaClient.java
@@ -1,21 +1,16 @@
 package dev.louis.nebula;
 
 import dev.louis.nebula.api.NebulaPlayer;
-import dev.louis.nebula.config.NebulaConfig;
 import dev.louis.nebula.networking.SynchronizeManaAmountS2CPacket;
 import dev.louis.nebula.networking.SynchronizeSpellsS2CPacket;
-import me.shedaniel.autoconfig.AutoConfig;
-import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 
 public class NebulaClient implements ClientModInitializer {
-    public static NebulaConfig config;
     @Override
     public void onInitializeClient() {
         registerPacketReceivers();
-        registerAutoConfigIfInstalled();
     }
 
     private void registerPacketReceivers() {
@@ -26,14 +21,5 @@ public class NebulaClient implements ClientModInitializer {
         });
         //Register the ManaAmount Packet.
         ClientPlayConnectionEvents.INIT.register(((handler, client) -> ClientPlayNetworking.registerReceiver(SynchronizeManaAmountS2CPacket.PACKET_TYPE, SynchronizeManaAmountS2CPacket::receive)));
-    }
-
-    private void registerAutoConfigIfInstalled() {
-        try {
-            AutoConfig.register(NebulaConfig.class, JanksonConfigSerializer::new);
-            config = AutoConfig.getConfigHolder(NebulaConfig.class).getConfig();
-        } catch (Exception exception) {
-            Nebula.LOGGER.debug("Couldn't register Cloth Config, if Cloth Config is installed this should not happen.");
-        }
     }
 }
