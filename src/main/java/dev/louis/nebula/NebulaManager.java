cdd0aa3434cd7f88d3b27ff3fdde9c9701d5ac1c
D1p4k <82884007+D1p4k@users.noreply.github.com>
2023-12-09 23:53:26 +0100

    Attempt at refactoring the NebulaManager and make it use entrypoints to make unhandled Mod Collisions impossible.

--- src/main/java/dev/louis/nebula/NebulaManager.java
+++ src/main/java/dev/louis/nebula/NebulaManager.java
@@ -1,69 +1,114 @@
 package dev.louis.nebula;
 
+import dev.louis.nebula.api.manager.entrypoint.RegisterManaManagerEntrypoint;
+import dev.louis.nebula.api.manager.entrypoint.RegisterSpellManagerEntrypoint;
+import dev.louis.nebula.api.manager.registerable.ManaManagerRegistrableView;
+import dev.louis.nebula.api.manager.registerable.SpellManagerRegistrableView;
 import dev.louis.nebula.mana.manager.ManaManager;
 import dev.louis.nebula.mana.manager.NebulaManaManager;
 import dev.louis.nebula.spell.manager.NebulaSpellManager;
 import dev.louis.nebula.spell.manager.SpellManager;
 import net.fabricmc.loader.api.FabricLoader;
-import net.fabricmc.loader.api.metadata.ModMetadata;
+import net.fabricmc.loader.api.ModContainer;
+import net.fabricmc.loader.api.entrypoint.EntrypointContainer;
 import net.minecraft.entity.player.PlayerEntity;
 
-public class NebulaManager {
-    private static final String JSON_KEY_CONTAINS_MANA_MANAGER = "nebula:contains_mana_manager";
-    private static final String JSON_KEY_CONTAINS_SPELL_MANAGER = "nebula:contains_spell_manager";
-    public static final NebulaManager INSTANCE = new NebulaManager();
-    private NebulaManager(){}
+import java.util.List;
 
-    private ManaManager.Factory<?> manaManagerFactory;
-    private SpellManager.Factory<?> spellManagerFactory;
-    private boolean loadManaManager = true;
-    private boolean loadSpellManager = true;
-    public void init() {
-        load();
-        if(loadManaManager)registerManaManagerFactory(NebulaManaManager::new);
-        if(loadSpellManager)registerSpellManagerFactory(NebulaSpellManager::new);
-    }
-    private void load() {
-        FabricLoader.getInstance().getAllMods().forEach(mod -> {
-            final ModMetadata metadata = mod.getMetadata();
-            if(metadata.getId().equals(Nebula.MOD_ID))return;
-            var loadManaManager = metadata.getCustomValue(JSON_KEY_CONTAINS_MANA_MANAGER);
-            if (loadManaManager != null && loadManaManager.getAsBoolean()) this.loadManaManager = false;
-            var loadSpellManager = metadata.getCustomValue(JSON_KEY_CONTAINS_SPELL_MANAGER);
-            if (loadSpellManager != null && loadSpellManager.getAsBoolean()) this.loadSpellManager = false;
-        });
+public class NebulaManager implements ManaManagerRegistrableView, SpellManagerRegistrableView {
+    private static ModContainer manaManagerMod;
+    private static ManaManager.Factory<?> manaManagerFactory;
+    private static ModContainer spellManagerMod;
+    private static SpellManager.Factory<?> spellManagerFactory;
+    private static boolean isLocked = false;
+
+    private NebulaManager() {}
+
+    public static void init() {
+        if(NebulaManager.isLocked) throw new IllegalStateException("Registration of Managers is locked!");
+        NebulaManager nebulaManager = new NebulaManager();
+        nebulaManager.runEntrypointsOrThrow();
+        nebulaManager.lock();
     }
 
-    public void registerManaManagerFactory(ManaManager.Factory<?> manaManager) {
-        if (manaManager == null) {
-            throw new NullPointerException("Attempt to register a NULL ManaManager");
-        }
-        if (this.manaManagerFactory != null) {
-            throw new UnsupportedOperationException("A second ManaManager attempted to register. Multiple ManaManagers are not supported.");
-        }
-        this.manaManagerFactory = manaManager;
-    }
-
-    public void registerSpellManagerFactory(SpellManager.Factory<?> spellKnowledgeManager) {
-        if (spellKnowledgeManager == null) {
-            throw new NullPointerException("Attempt to register a NULL SpellManager");
-        }
-        if (this.spellManagerFactory != null) {
-            throw new UnsupportedOperationException("A SpellManager plug-in attempted to register. Multiple SpellManagers are not supported.");
-        }
-        this.spellManagerFactory = spellKnowledgeManager;
-    }
-
-    public ManaManager.Factory<?> getManaManagerFactory() {
+    public static ManaManager.Factory<?> getManaManagerFactory() {
         return manaManagerFactory;
     }
-    public SpellManager.Factory<?> getSpellManagerFactory() {
+
+    public static SpellManager.Factory<?> getSpellManagerFactory() {
         return spellManagerFactory;
     }
+
     public static ManaManager createManaManager(PlayerEntity player) {
-        return INSTANCE.getManaManagerFactory().createPlayerManaManager(player);
+        return getManaManagerFactory().createPlayerManaManager(player);
     }
+
     public static SpellManager createSpellManager(PlayerEntity player) {
-        return INSTANCE.getSpellManagerFactory().createSpellKnowledgeManager(player);
+        return getSpellManagerFactory().createSpellKnowledgeManager(player);
+    }
+
+    public void lock() {
+        if(spellManagerFactory == null) spellManagerFactory = NebulaSpellManager::new;
+        if(manaManagerFactory == null) manaManagerFactory = NebulaManaManager::new;
+        isLocked = true;
+    }
+
+    @Override
+    public void registerManaManagerFactory(ManaManager.Factory<?> manaManagerFactory) {
+        NebulaManager.manaManagerFactory = manaManagerFactory;
+    }
+
+    @Override
+    public void registerSpellManagerFactory(SpellManager.Factory<?> spellManagerFactory) {
+        NebulaManager.spellManagerFactory = spellManagerFactory;
+    }
+
+    /**
+     * Throws an exception if multiple mods want to override the ManaManager or SpellManager.
+     */
+    private void runEntrypointsOrThrow() {
+        var manaManagerEntrypoints = FabricLoader.getInstance().getEntrypointContainers("registerManaManager", RegisterManaManagerEntrypoint.class)
+                .stream().filter(container -> container.getEntrypoint().shouldRegister()).toList();
+
+        var spellManagerEntrypoints = FabricLoader.getInstance().getEntrypointContainers("registerSpellManager", RegisterSpellManagerEntrypoint.class)
+                .stream().filter(container -> container.getEntrypoint().shouldRegister()).toList();
+
+        var manaManagerMods = manaManagerEntrypoints.stream().map(EntrypointContainer::getProvider).toList();
+        var spellManagerMods = spellManagerEntrypoints.stream().map(EntrypointContainer::getProvider).toList();
+
+        if(manaManagerEntrypoints.size() > 1 && spellManagerEntrypoints.size() > 1) {
+            throw new IllegalStateException("Multiple Mods want to override the ManaManager and SpellManager!\n " +
+                    "Mods overriding ManaManager: " + manaManagerMods + ",\n" +
+                    "Mods overriding SpellManager: " + spellManagerMods);
+        }
+
+        if(manaManagerEntrypoints.size() > 1) {
+            throw new IllegalStateException("Multiple Mods want to override the ManaManager! Mods: " + manaManagerMods);
+        }
+
+        if(spellManagerEntrypoints.size() > 1) {
+            throw new IllegalStateException("Multiple Mods want to override the SpellManager! Mods: " + spellManagerMods);
+        }
+
+        runEntryPoints(getFirstOrNull(manaManagerEntrypoints), getFirstOrNull(spellManagerEntrypoints));
+    }
+
+    private void runEntryPoints(
+            EntrypointContainer<RegisterManaManagerEntrypoint> manaManagerEntrypointEntrypointContainer,
+            EntrypointContainer<RegisterSpellManagerEntrypoint> spellManagerEntrypointEntrypointContainer
+    ) {
+        if(manaManagerEntrypointEntrypointContainer != null) {
+            manaManagerEntrypointEntrypointContainer.getEntrypoint().registerSpell(this);
+            NebulaManager.manaManagerMod = manaManagerEntrypointEntrypointContainer.getProvider();
+        }
+
+        if(spellManagerEntrypointEntrypointContainer != null) {
+            spellManagerEntrypointEntrypointContainer.getEntrypoint().registerSpell(this);
+            NebulaManager.spellManagerMod = spellManagerEntrypointEntrypointContainer.getProvider();
+        }
+    }
+
+    private static <T> T getFirstOrNull(List<T> list) {
+        return list.isEmpty() ? null : list.get(0);
     }
 }
