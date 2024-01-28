e4868c4ce5a7dc71a40c27e307e8e029c79e2620
D1p4k <82884007+D1p4k@users.noreply.github.com>
2024-01-29 00:59:41 +0100

    First Api Breakage! :) Becuase we need it

--- src/main/java/dev/louis/nebula/api/manager/spell/registerable/SpellManagerRegistrableView.java
+++ src/main/java/dev/louis/nebula/api/manager/spell/registerable/SpellManagerRegistrableView.java
@@ -1,13 +1,14 @@
 package dev.louis.nebula.api.manager.spell.registerable;
 
 import dev.louis.nebula.api.manager.spell.SpellManager;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.util.Identifier;
+
+import java.util.function.Consumer;
 
 public interface SpellManagerRegistrableView {
         void registerSpellManager(
                 SpellManager.Factory<?> manaManagerFactory,
-                Identifier packetId,
-                ClientPlayNetworking.PlayChannelHandler channelHandler
+                Identifier spellPacketId,
+                Consumer<Identifier> spellPacketRegisterer
         );
 }
