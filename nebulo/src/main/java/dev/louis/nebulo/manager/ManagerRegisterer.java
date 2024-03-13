5af59c0f77afe6e6a0100737d308707533b1296d
D1p4k <82884007+D1p4k@users.noreply.github.com>
2024-03-13 17:34:37 +0100

    Remove SpellEntity.java

--- nebulo/src/main/java/dev/louis/nebulo/manager/ManagerRegisterer.java
+++ nebulo/src/main/java/dev/louis/nebulo/manager/ManagerRegisterer.java
@@ -4,13 +4,8 @@ import dev.louis.nebula.api.manager.mana.entrypoint.RegisterManaManagerEntrypoin
 import dev.louis.nebula.api.manager.mana.registerable.ManaManagerRegistrableView;
 import dev.louis.nebula.api.manager.spell.entrypoint.RegisterSpellManagerEntrypoint;
 import dev.louis.nebula.api.manager.spell.registerable.SpellManagerRegistrableView;
-import dev.louis.nebula.manager.mana.NebulaManaManager;
-import dev.louis.nebula.manager.spell.NebulaSpellManager;
-import dev.louis.nebula.networking.SyncManaS2CPacket;
-import dev.louis.nebula.networking.UpdateSpellCastabilityS2CPacket;
 import dev.louis.nebulo.manager.mana.NebuloManaManager;
 import dev.louis.nebulo.manager.spell.NebuloSpellManager;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 
 public class ManagerRegisterer implements RegisterManaManagerEntrypoint, RegisterSpellManagerEntrypoint {
     @Override
@@ -19,18 +14,8 @@ public class ManagerRegisterer implements RegisterManaManagerEntrypoint, Registe
     }
 
     @Override
-    public void registerManaPacketReceiver() {
-        ClientPlayNetworking.registerGlobalReceiver(SyncManaS2CPacket.TYPE, NebulaManaManager::receiveSync);
-    }
-
-    @Override
     public void registerSpellManager(SpellManagerRegistrableView spellManagerRegistrableView) {
         spellManagerRegistrableView.registerSpellManager(NebuloSpellManager::new);
-    }
-
-    @Override
-    public void registerSpellPacketReceiver() {
-        ClientPlayNetworking.registerGlobalReceiver(UpdateSpellCastabilityS2CPacket.TYPE, NebulaSpellManager::receiveSync);
     }
 
     @Override
