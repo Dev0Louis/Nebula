c8087b24395b4092ef44a48de7f90fa1554391da
D1p4k <82884007+D1p4k@users.noreply.github.com>
2024-07-10 22:41:41 +0200

    Fix Payload Registration Not Working Correct Under Circumstances.

--- src/main/java/dev/louis/nebula/Nebula.java
+++ src/main/java/dev/louis/nebula/Nebula.java
@@ -5,6 +5,8 @@ import dev.louis.nebula.api.spell.SpellType;
 import dev.louis.nebula.command.NebulaCommand;
 import dev.louis.nebula.manager.NebulaManager;
 import dev.louis.nebula.networking.c2s.SpellCastPayload;
+import dev.louis.nebula.networking.s2c.SyncManaPayload;
+import dev.louis.nebula.networking.s2c.UpdateSpellCastabilityPayload;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
@@ -26,7 +28,10 @@ public class Nebula implements ModInitializer {
     }
 
     public void registerPacketReceivers() {
+        PayloadTypeRegistry.playS2C().register(UpdateSpellCastabilityPayload.ID, UpdateSpellCastabilityPayload.CODEC);
+        PayloadTypeRegistry.playS2C().register(SyncManaPayload.ID, SyncManaPayload.CODEC);
         PayloadTypeRegistry.playC2S().register(SpellCastPayload.ID, SpellCastPayload.CODEC);
+
         ServerPlayNetworking.registerGlobalReceiver(SpellCastPayload.ID, SpellCastPayload::receive);
     }
 }
--- src/main/java/dev/louis/nebula/NebulaClient.java
+++ src/main/java/dev/louis/nebula/NebulaClient.java
@@ -6,7 +6,6 @@ import dev.louis.nebula.networking.s2c.SyncManaPayload;
 import dev.louis.nebula.networking.s2c.UpdateSpellCastabilityPayload;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 
 public class NebulaClient implements ClientModInitializer {
 
@@ -16,10 +15,9 @@ public class NebulaClient implements ClientModInitializer {
     }
 
     public void registerPacketReceivers() {
-        PayloadTypeRegistry.playS2C().register(UpdateSpellCastabilityPayload.ID, UpdateSpellCastabilityPayload.CODEC);
+
         ClientPlayNetworking.registerGlobalReceiver(UpdateSpellCastabilityPayload.ID, NebulaSpellManager::receiveSync);
 
-        PayloadTypeRegistry.playS2C().register(SyncManaPayload.ID, SyncManaPayload.CODEC);
         ClientPlayNetworking.registerGlobalReceiver(SyncManaPayload.ID, NebulaManaManager::receiveSync);
     }
 }
